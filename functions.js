4bc040acb4afeda2aadd20847efaab6754aaa1ae
Tyler <139372160+tpinzzz@users.noreply.github.com>
2024-06-21 00:57:15 -0400

    Feat: created etch-a-sketch html, css and javascript

---
+++ functions.js
@@ -0,0 +1,66 @@
+//Create a webpage with a 16x16 grid of square divs
+
+
+const canvascontainer = document.createElement("div");
+canvascontainer.classList.add(`canvascontainer`);
+
+body = document.querySelector("body");
+body.appendChild(canvascontainer);
+
+let numberofsquares = 16;
+
+const adjustcanvas = document.querySelector(".adjustcanvas");
+
+createGrid(numberofsquares);
+
+function createGrid(numberofsquares){
+
+    for (let i = 0; i < numberofsquares; i++){
+    //create div for each row 
+    const r = document.createElement("div");
+    r.classList.add(`rowcontainer`);
+    canvascontainer.appendChild(r);
+
+        for (let j = 0; j < numberofsquares; j++) {
+            const square = document.createElement("div");
+            square.textContent = "";
+            //add class for each row and column
+            const list = square.classList;
+            list.add(`rowitem`);
+            list.add(`col${j}`);
+
+            //row = document.querySelector("body");
+            r.appendChild(square);
+
+
+            //add event listeners for hovering functionality to each square
+            square.addEventListener("mouseenter", (e) => {
+                e.target.style.background = "blue";
+            })
+        }
+
+    }
+}
+
+
+
+
+
+adjustcanvas.addEventListener("click", () => {
+    let numberofsquares = prompt("Please enter a number between 0 and 100");
+    if (numberofsquares !== null) {
+        numberofsquares = parseInt(numberofsquares);
+        if (!isNaN(numberofsquares) && numberofsquares > 0 && numberofsquares <= 100) {
+            console.log("test")
+            canvascontainer.innerHTML = '';
+            createGrid(numberofsquares);
+        } else {
+            alert("Please enter a valid number between 1 and 100.");
+        }
+    }
+});
+
+
+
+
+
